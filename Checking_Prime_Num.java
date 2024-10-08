960267c622caabfa141ca9d3c8911a9d77d74d20
sakthi <sakthivel.vipsk@gmail.com>
2024-10-08 13:33:27 +0530

    Prime_num program

---
+++ Checking_Prime_Num.java
@@ -0,0 +1,20 @@
+package org.JavaArrayPrograms;
+
+public class Checking_Prime_Num {
+    public static void main(String[] args) {
+        int n=17;
+        boolean isPrime=true;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime){
+            System.out.println("Prime Number");
+        }
+        else {
+            System.out.println("Not Prime Number");
+        }
+    }
+}
