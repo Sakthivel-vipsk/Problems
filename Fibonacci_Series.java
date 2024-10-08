4c9ead51cdeda79b0708142aec39cb8b25c3ff5f
sakthi <sakthivel.vipsk@gmail.com>
2024-10-08 13:22:29 +0530

    Fibonacci series program

---
+++ Fibonacci_Series.java
@@ -0,0 +1,14 @@
+package org.JavaArrayPrograms;
+
+public class Fibonacci_Series {
+    public static void main(String[] args) {
+        int n=10, first =0, second = 1;
+        System.out.print(first +", "+second);
+        for(int i=2;i<n;i++){
+            int next = first+second;
+            System.out.print(", "+next);
+            first = second;
+            second = next;
+        }
+    }
+}
