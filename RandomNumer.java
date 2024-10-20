b1ca9a1d28b1d964b151745af8f46fd724ca6c09
sakthi <sakthivel.vipsk@gmail.com>
2024-10-20 11:37:36 +0530

    RandomNumer

---
+++ RandomNumer.java
@@ -0,0 +1,10 @@
+package org.JavaArrayPrograms;
+
+import java.util.Random;
+
+public class RandomNumer {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        System.out.println(rand.nextInt(10));
+    }
+}
